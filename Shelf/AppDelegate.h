8b4a736d92d3d6df450ad654535834c99afbc75f
Tom Insam <tom@movieos.org>
2013-12-03 18:50:27 +0000

    stub xcode project

---
+++ Shelf/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Shelf
+//
+//  Created by Tom Insam on 2013/12/03.
+//  Copyright (c) 2013 Tom Insam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Shelf/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Shelf
+//
+//  Created by Tom Insam on 2013/12/03.
+//  Copyright (c) 2013 Tom Insam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
